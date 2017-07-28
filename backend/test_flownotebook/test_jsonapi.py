4f29568a0431cc053ed59088c4f84652655c83ed
Frapples <isfrapples@outlook.com>
2017-07-28 11:30:35 +0800

    ADD: 分类添加获取的单元测试

--- backend/test_flownotebook/test_jsonapi.py
+++ backend/test_flownotebook/test_jsonapi.py
@@ -4,7 +4,7 @@ sys.path.append("..")
 from flownotebook import app
 
 
-class Test:
+class TestUser:
     def setup(self):
         app.config['TESTING'] = True
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
@@ -12,39 +12,120 @@ class Test:
         self.app = app.test_client(use_cookies=True)
 
     def test_user(self):
-        self.user_register()
-        self.user_login()
-        self.is_login()
+        self.user_register(self.app)
+        self.user_login(self.app)
+        self.is_login(self.app)
 
-    def user_register(self):
+    @staticmethod
+    def user_register(app):
         url = '/json_api/user/register'
         post_data = {'username': 'testname', 'password': "12345678"}
-        rv = self.app.post(url, data=post_data)
+        rv = app.post(url, data=post_data)
         result = json.loads(rv.data.decode())
 
         assert result['success']
 
-        rv = self.app.post(url, data=post_data)
+        rv = app.post(url, data=post_data)
         result = json.loads(rv.data.decode())
         assert not result['success'] and result['reason'] == "USERNAME_EXISTED"
 
-    def user_login(self):
+    @staticmethod
+    def user_login(app):
         url = '/json_api/user/login'
 
-        rv = self.app.post(url, data={'username': 'testname',
-                                      'password': '12345678'})
+        rv = app.post(url, data={'username': 'testname',
+                                 'password': '12345678'})
         result = json.loads(rv.data.decode())
         assert result['success']
 
-        rv = self.app.post(url, data={'username': 'testname',
-                                      'password': '12345688'})
+        rv = app.post(url, data={'username': 'testname',
+                                 'password': '12345688'})
         result = json.loads(rv.data.decode())
         assert not result['success']
 
-    def is_login(self):
-        rv = self.app.post("/json_api/user/is_login")
+    @staticmethod
+    def is_login(app):
+        rv = app.post("/json_api/user/is_login")
         result = json.loads(rv.data.decode())
         assert result['is_login']
 
     def teardown(self):
         pass
+
+
+class TestNote:
+    def setup(self):
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        print('setup>>>')
+        self.app = app.test_client(use_cookies=True)
+
+        TestUser.user_login(self.app)
+
+    def test_note(self):
+        categories = {
+            'A': {
+                'AA': {
+                    'AAA': {
+                    },
+                    'BBB': {
+                    },
+                    'CCC': {
+                    }
+                },
+                'BB': {
+                },
+                'CC': {
+                }
+            },
+            'B': {
+            },
+            'C': {
+            }
+        }
+
+        def cmp(a, b):
+            if len(a) != len(b):
+                return False
+            for k in a:
+                if k not in b:
+                    return False
+                if not cmp(a[k], b[k]):
+                    return False
+            return True
+
+        self.add_categories(self.app, categories)
+        assert cmp(categories, self.get_all_categories(self.app))
+
+    @staticmethod
+    def add_categories(app, categories):
+
+        def add(category, pid):
+            for name in category:
+                url = '/json_api/note/category_add'
+                rv = app.post(url, data={'parent_id': pid, 'name': name})
+                result = json.loads(rv.data.decode())
+                print(result)
+                assert result['success']
+                add(category[name], pid=result['id'])
+
+        add(categories, -1)
+
+    @staticmethod
+    def get_all_categories(app):
+        def fetch(id):
+            url = '/json_api/note/category_children'
+            rv = app.post(url, data={'id': id})
+            result = json.loads(rv.data.decode())
+            assert result['success']
+            return result['data']
+
+        def get(id):
+            result = dict()
+
+            nodes = fetch(id)
+            for n in nodes:
+                result[n['name']] = get(n['id'])
+            return result
+
+        return get(-1)
